152e7944633eec248f8985cf6253421fb3138d31
JaNg2333 <1120357239@qq.com>
2023-07-28 16:41:43 +0800

    2023.0728 update dartblur.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from PIL import Image
 
 def letterbox_image(image, size):
     ih, iw, _ = np.shape(image)
@@ -23,3 +23,52 @@ def get_lr(optimizer):
 def preprocess_input(image):
     image -= np.array((104, 117, 123), np.float32)
     return image
+
+
+def process_img(image, target, input_shape):
+    iw, ih = image.size
+    h, w = input_shape, input_shape
+    box = target
+
+    new_ar = w / h
+
+    if new_ar < 1:
+        nh = int(h)
+        nw = int(nh * new_ar)
+    else:
+        nw = int(w)
+        nh = int(nw / new_ar)
+    image = image.resize((nw, nh), Image.BICUBIC)
+
+    dx = int(w - nw)
+    dy = int(h - nh)
+    new_image = Image.new('RGB', (w, h), (128, 128, 128))
+    new_image.paste(image, (dx, dy))
+    image = new_image
+    image_data = np.array(image, np.uint8)
+
+    if len(box) > 0:
+        box[:, [0, 2]] = box[:, [0, 2]] * nw / iw + dx
+        box[:, [1, 3]] = box[:, [1, 3]] * nh / ih + dy
+        center_x = (box[:, 0] + box[:, 2]) / 2
+        center_y = (box[:, 1] + box[:, 3]) / 2
+        box = box[np.logical_and(np.logical_and(center_x > 0, center_y > 0),
+                                    np.logical_and(center_x < w, center_y < h))]
+        box[:, 0:4][box[:, 0:4] < 0] = 0
+        box[:, [0, 2]][box[:, [0, 2]] > w] = w
+        box[:, [1, 3]][box[:, [1, 3]] > h] = h
+
+        box_w = box[:, 2] - box[:, 0]
+        box_h = box[:, 3] - box[:, 1]
+        box = box[np.logical_and(box_w > 1, box_h > 1)]  # discard invalid box
+    # print(box)
+    box[:, [0, 2]] = box[:, [0, 2]] / float(w)
+    box[:, [1, 3]] = box[:, [1, 3]] / float(h)
+    box_data = box
+    # print(box_data)
+    # modified here
+    image_data = np.transpose(preprocess_input(np.array(image_data, np.float32)), (2, 0, 1))
+
+    return image_data, box_data, dx, dy
+
+ 
